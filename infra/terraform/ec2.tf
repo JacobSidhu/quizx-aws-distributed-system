08723c5237962fb4627fc5b74e15dd265b440f53
Jacob Sidhu <148295294+JacobSidhu@users.noreply.github.com>
2026-05-31 21:16:36 +0000

    Add defining resource aws instance

---
+++ infra/terraform/ec2.tf
@@ -0,0 +1,14 @@
+// Creates EC2 instance
+resource "aws_instance" "web" {
+  ami                         = var.aws_ami_id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public.id
+  associate_public_ip_address = true
+  vpc_security_group_ids      = [aws_security_group.quizx_sg.id]
+
+  key_name = aws_key_pair.quizx_key.key_name
+
+  tags = {
+    Name = "${var.project_name}-web"
+  }
+}
