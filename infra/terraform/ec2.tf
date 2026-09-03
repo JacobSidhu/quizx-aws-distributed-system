320b673b053e05e8a3cb04b4002b9e7f46916575
Jacob Sidhu <148295294+JacobSidhu@users.noreply.github.com>
2026-09-03 15:37:48 +0000

    Moved aws key pair into ec2 tf file

--- infra/terraform/ec2.tf
+++ infra/terraform/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "question_app" {
   ami                         = var.aws_ami_id
   instance_type               = var.instance_type
   subnet_id                   = aws_subnet.public.id
-  associate_public_ip_address = true
+  associate_public_ip_address = false
   vpc_security_group_ids      = [aws_security_group.question_app_sg.id]
 
   key_name = aws_key_pair.quizx_key.key_name
@@ -19,7 +19,7 @@ resource "aws_instance" "submit_app" {
   ami                         = var.aws_ami_id
   instance_type               = var.instance_type
   subnet_id                   = aws_subnet.public.id
-  associate_public_ip_address = true
+  associate_public_ip_address = false
   vpc_security_group_ids      = [aws_security_group.submit_app_sg.id]
 
   key_name = aws_key_pair.quizx_key.key_name
@@ -29,4 +29,16 @@ resource "aws_instance" "submit_app" {
   tags = {
     Name = "${var.project_name}-submit-app"
   }
+}
+
+// ----------------------------------------------------------
+// KEY PAIR
+// ----------------------------------------------------------
+resource "aws_key_pair" "quizx_key" {
+  key_name   = coalesce(var.ec2_key_name, "${var.project_name}-ec2-key")
+  public_key = var.ssh_public_key
+
+  tags = merge(local.common_tags, {
+    Name = coalesce(var.ec2_key_name, "${local.name_prefix}-ec2-key")
+  })
 }
