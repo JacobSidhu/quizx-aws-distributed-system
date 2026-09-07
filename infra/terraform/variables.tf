35f46b373447cda50339f4be54c5912e07b92d7b
Jacob Sidhu <148295294+JacobSidhu@users.noreply.github.com>
2026-09-07 12:59:26 +0000

    ADD: variables for second availability zone and subnet

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -10,10 +10,16 @@ variable "aws_region" {
   default     = "eu-west-2"
 }
 
-variable "availability_zone" {
+variable "availability_zone_1" {
   description = "AWS availability zone for resource deployment"
   type        = string
   default     = "eu-west-2a"
+}
+
+variable "availability_zone_2" {
+  description = "AWS availability zone for resource deployment"
+  type        = string
+  default     = "eu-west-2b"
 }
 
 variable "vpc_cidr" {
@@ -61,12 +67,17 @@ variable "submit_app_cidr" {
   default     = null
 }
 
-variable "public_subnet_cidr" {
+variable "public_subnet_1_cidr" {
   description = "CIDR block for the public subnet"
   type        = string
   default     = "10.0.1.0/24"
 }
 
+variable "public_subnet_2_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+  default     = "10.0.2.0/24"
+}
 // ----------------------------------------------------------
 // ---------Port variables.
 // ----------------------------------------------------------
